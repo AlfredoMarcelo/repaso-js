ea17c6f3ec223f005dfa50ece3957e75cf7cf19e
Alfredo Supanta <alfredo.supanta.26@gmail.com>
2022-07-05 09:29:17 -0400

    se agregar 4 methodos para arreglos

--- index.js
+++ index.js
@@ -25,10 +25,33 @@ console.log(miArregloAnidado[0]);//[1 , 2 , 3]
 // Pintar un arreglo dentro de otro arreglo
 console.log(miArregloAnidado[0][1]);//2
 
-// PUSH() METHOD
+console.log("METODOS DE ARRAY, .push() .pop() .shift() .unshift()")
+
+// PUSH() METHOD, agrega al final
 
 let estaciones = ["Invierno", "Otoño", "Primaver"];
 console.log(estaciones);//["Invierno", "Otoño", "Primaver"]
 
 estaciones.push("Verano");
 console.log(estaciones);//["Invierno", "Otoño", "Primaver", "Verano"]
+
+
+//.POP() Method, quita el utlimo valor del arreglo y se puede guardar en una variable
+
+estaciones = ["Invierno", "Otoño", "Primavera", "Verano"];
+let estacion = estaciones.pop();
+console.log(estaciones);//["Invierno", "Otoño", "Primavera"]
+
+console.log(estacion);// "Verano"
+
+// .SHIFT Method, quita el valor al comienzo del array
+
+estaciones = ["Invierno", "Otoño", "Primavera", "Verano"];
+estaciones.shift();
+console.log(estaciones)// ["Otoño", "Primavera", "Verano"]
+
+// .UNSHIFT Method, agrega un valor al comienzo del array
+
+estaciones = ["Invierno", "Otoño", "Primavera"];
+estaciones.unshift("Verano");
+console.log(estaciones)// ["Verano","Invierno","Otoño", "Primavera", ]
